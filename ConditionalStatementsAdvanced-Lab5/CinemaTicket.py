0631aea9560e1ccfe3f02a1bb52bcf43a2190d73
Georgi Kirilov <106103126+georgi1214@users.noreply.github.com>
2022-08-06 10:34:36 +0300

    Add files via upload

---
+++ ConditionalStatementsAdvanced-Lab5/CinemaTicket.py
@@ -0,0 +1,8 @@
+weekDay = str(input())
+
+if weekDay == 'Monday' or weekDay == 'Tuesday' or weekDay == 'Friday':
+    print(12)
+elif weekDay == 'Wednesday' or weekDay == 'Thursday':
+    print(14)
+else:
+    print(16)
