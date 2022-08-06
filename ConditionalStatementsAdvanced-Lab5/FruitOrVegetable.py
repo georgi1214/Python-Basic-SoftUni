109281d8b54075365ff1437c212d46da5fb9457f
Georgi Kirilov <106103126+georgi1214@users.noreply.github.com>
2022-08-06 10:35:14 +0300

    Add files via upload

---
+++ ConditionalStatementsAdvanced-Lab5/FruitOrVegetable.py
@@ -0,0 +1,8 @@
+kind = input()
+
+if kind == 'banana' or kind == 'apple' or kind == 'kiwi' or kind == 'cherry' or kind == 'lemon' or kind == 'grapes':
+    print('fruit')
+elif kind == 'tomato' or kind == 'cucumber' or kind == 'pepper' or kind == 'carrot':
+    print('vegetable')
+else:
+    print('unknown')
