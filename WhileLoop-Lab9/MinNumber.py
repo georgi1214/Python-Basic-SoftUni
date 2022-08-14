2e74e4cc44b6fb85c5d2a5314b0e88901d9f655c
Georgi Kirilov <106103126+georgi1214@users.noreply.github.com>
2022-08-14 16:57:24 +0300

    Add files via upload

---
+++ WhileLoop-Lab9/MinNumber.py
@@ -0,0 +1,12 @@
+import sys
+
+command = int(input())
+
+min_number = sys.maxsize
+
+while command != 'Stop':
+    current_num = int(command)
+    if current_num < min_number:
+        min_number = current_num
+    command = input()
+print(min_number)
