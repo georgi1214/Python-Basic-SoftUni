957d1ae85cfa69f9f2a89bed32bc576397d60c49
Georgi Kirilov <106103126+georgi1214@users.noreply.github.com>
2022-08-15 22:44:13 +0300

    Add files via upload

---
+++ WhileLoop-Lab9/SumNumbers.py
@@ -0,0 +1,8 @@
+number = int(input())
+
+sum_of_numbers = int(input())
+
+while sum_of_numbers < number:
+    current_number = int(input())
+    sum_of_numbers += current_number
+print(sum_of_numbers)
