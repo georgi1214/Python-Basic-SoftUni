a31b56b7613c1ce4dce37ed2177a15766117708e
Georgi Kirilov <106103126+georgi1214@users.noreply.github.com>
2022-08-21 09:03:36 +0300

    Add files via upload

---
+++ NestedLoops-Exercise12/EqualSumsEvenOddPosition.py
@@ -0,0 +1,17 @@
+start_num = int(input())
+end_num = int(input())
+
+for i in range(start_num, end_num + 1):
+    current_num_str = str(i)
+    even_sum = 0
+    odd_sum = 0
+
+    for j in range(0, len(current_num_str)):
+        digit = int(current_num_str[j])
+        if j % 2 == 0:
+            even_sum = even_sum + digit
+        else:
+            odd_sum = odd_sum + digit
+
+    if even_sum == odd_sum:
+        print(current_num_str, end=" ")
